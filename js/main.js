3a90a7b21f6df566d500db6ae2e9d43b03e0b4f9
Damian Martinez <damian.e.martinez@gmail.com>
2023-03-09 17:30:13 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,13 +3,12 @@ function init(){
         fetch('https://api.igdb.com/v4/games/', {
         method: 'POST',
         headers: {
-            'Client-ID': 'qhf04rd5op5wr9mvdaq812wmmbo9eg',
-            'Authorization': 'Bearer gb4cijdgjmowfz3g2djesx54hk37pq'
+            
         },
         body: JSON.stringify({ "id": 78912 })
-    })
-    .then(response => response.json())
-    .then(response => console.log(JSON.stringify(response)))
+        })
+        .then(response => response.json())
+        .then(response => console.log(JSON.stringify(response)))
     } catch (error) {
         console.log("No se pudo :c");
     }
