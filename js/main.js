c09d82ce4bec2dfa794f4598db7aa9c1478f243f
Damian Martinez <damian.e.martinez@gmail.com>
2023-03-14 13:50:04 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,82 +1,58 @@
-/* ===================================================================
- * TypeRite - Main JS
- *
- *
- * ------------------------------------------------------------------- */
-
+/*====================================================================
+ !                  Rincon del Gamer - Main JS
+ *====================================================================*/
 (function($) {
-
     "use strict";
-    
     var cfg = {
-        scrollDuration : 800, // smoothscroll duration
-        mailChimpURL   : ''   // mailchimp url
+        scrollDuration : 800, // Smoothscroll duration in ms
+        mailChimpURL   : ''   // Mailchimp url
     },
 
     $WIN = $(window);
-
-    // Add the User Agent to the <html>
-    // will be used for IE10/IE11 detection (Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0; rv:11.0))
+    //* Add the User Agent to the <html>
     var doc = document.documentElement;
     doc.setAttribute('data-useragent', navigator.userAgent);
 
-
-   /* preloader
-    * -------------------------------------------------- */
+    /* Preloader */
     var ssPreloader = function() {
-        
         $("html").addClass('ss-preload');
-
         $WIN.on('load', function() {
+            // Force page scroll position to top at page refresh
+            $('html, body').animate({ scrollTop: 0 }, 'normal');
 
-            //force page scroll position to top at page refresh
-            // $('html, body').animate({ scrollTop: 0 }, 'normal');
-
-            // will first fade out the loading animation 
+            // Will first fade out the loading animation 
             $("#loader").fadeOut("slow", function() {
-                // will fade out the whole DIV that covers the website.
+                // Will fade out the whole DIV that covers the website.
                 $("#preloader").delay(300).fadeOut("slow");
             }); 
             
-            // for hero content animations 
+            // For content animations 
             $("html").removeClass('ss-preload');
             $("html").addClass('ss-loaded');
-        
         });
     };
 
-
-   /* search
-    * ------------------------------------------------------ */
+   /* Search */
     var ssSearch = function() {
-            
         var searchWrap = $('.search-block'),
             searchField = searchWrap.find('.search-field'),
             closeSearch = searchWrap.find('.search-close'),
             searchTrigger = $('.search-trigger'),
             siteBody = $('body');
 
-
         searchTrigger.on('click', function(e) {
-            
             e.preventDefault();
             e.stopPropagation();
-        
             var $this = $(this);
-        
             siteBody.addClass('search-is-visible');
             setTimeout(function(){
                 searchWrap.find('.search-field').focus();
             }, 100);
-        
         });
 
         closeSearch.on('click', function(e) {
-
             var $this = $(this);
-        
             e.stopPropagation(); 
-        
             if(siteBody.hasClass('search-is-visible')){
                 siteBody.removeClass('search-is-visible');
                 setTimeout(function(){
@@ -95,18 +71,23 @@
             e.stopPropagation();
         });
             
-        searchField.attr({placeholder: 'Type Keywords', autocomplete: 'off'});
+        searchField.attr({placeholder: 'Escriba aquí', autocomplete: 'off'});
 
+        /*
+            TODO: Arrange the Search Fucntion to call API and request the respective Answers
+         */
+        searchField.on('keypress', function(e){
+            if(e.key === 'Enter'){
+                console.log('Buscando ' + this.value);
+            }
+        });
     };
 
-
-   /* menu
-    * ------------------------------------------------------ */
+    /* Menu */
     var ssMenu = function() {
-
         var menuToggle = $('.header__menu-toggle'),
             siteBody = $('body');
-    
+
         menuToggle.on('click', function(e) {
             e.preventDefault();
             e.stopPropagation();
@@ -115,9 +96,7 @@
         });
 
         $('.header__nav .has-children').children('a').on('click', function (e) {
-            
             e.preventDefault();
-
             $(this).toggleClass('sub-menu-is-open')
                 .next('ul')
                 .slideToggle(200)
@@ -128,17 +107,13 @@
                 .removeClass('sub-menu-is-open')
                 .next('ul')
                 .slideUp(200);
-
         });
     };
 
 
-   /* masonry
-    * ---------------------------------------------------- */ 
+    /* Masonry */ 
     var ssMasonryFolio = function () {
-        
         var containerBricks = $('.masonry');
-
         containerBricks.masonry({
             itemSelector: '.masonry__brick',
             columnWidth: '.grid-sizer',
@@ -146,45 +121,34 @@
             resize: true
         });
 
-        // layout Masonry after each image loads
+        // Layout Masonry after each image loads
         containerBricks.imagesLoaded().progress( function() {
             containerBricks.masonry('layout');
         });
-
     };
 
-   /* animate bricks
-    * ------------------------------------------------------ */
+    /* Animate bricks */
     var ssBricksAnimate = function() {
-
         var animateEl = $('.animate-this');
-
         $WIN.on('load', function() {
-
             setTimeout(function() {
                 animateEl.each(function(ctr) {
                     var el = $(this);
-                    
                     setTimeout(function() {
                         el.addClass('animated');
                     }, ctr * 200);
                 });
             }, 300);
-
         });
 
         $WIN.on('resize', function() {
-            // remove animation classes
+            // Remove animation classes
             animateEl.removeClass('animate-this animated');
         });
-
     };
 
-
-   /* slick slider
-    * ------------------------------------------------------ */
+   /* Slick slider */
     var ssSlickSlider = function() {
-
         var $gallery = $('.slider__slides').slick({
             arrows: false,
             dots: true,
@@ -200,20 +164,17 @@
         $('.slider__slide').on('click', function() {
             $gallery.slick('slickGoTo', parseInt($gallery.slick('slickCurrentSlide'))+1);
         });
-
     };
 
 
-   /* smooth scrolling
-    * ------------------------------------------------------ */
+   /* Smooth scrolling */
     var ssSmoothScroll = function() {
-        
         $('.smoothscroll').on('click', function (e) {
             var target = this.hash,
             $target    = $(target);
             
-                e.preventDefault();
-                e.stopPropagation();
+            e.preventDefault();
+            e.stopPropagation();
 
             $('html, body').stop().animate({
                 'scrollTop': $target.offset().top
@@ -223,32 +184,23 @@
                 if ($('body').hasClass('menu-is-open')) {
                     $('.header-menu-toggle').trigger('click');
                 }
-
                 window.location.hash = target;
             });
         });
-
     };
 
 
-   /* alert boxes
-    * ------------------------------------------------------ */
+   /* Alert boxes */
     var ssAlertBoxes = function() {
-
         $('.alert-box').on('click', '.alert-box__close', function() {
             $(this).parent().fadeOut(500);
-        }); 
-
+        });
     };
 
-
-   /* Back to Top
-    * ------------------------------------------------------ */
+    /* Back to Top */
     var ssBackToTop = function() {
-        
         var pxShow      = 500,
             goTopButton = $(".go-top")
-
         // Show or hide the button
         if ($(window).scrollTop() >= pxShow) goTopButton.addClass('link-is-visible');
 
@@ -262,10 +214,8 @@
     };
 
 
-   /* Initialize
-    * ------------------------------------------------------ */
+   /* Initialize */
     (function clInit() {
-
         ssPreloader();
         ssSearch();
         ssMenu();
@@ -275,7 +225,5 @@
         ssSmoothScroll();
         ssAlertBoxes();
         ssBackToTop();
-
     })();
-
 })(jQuery);
